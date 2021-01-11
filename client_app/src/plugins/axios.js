8edc4e5686163a5aefe4a63eca56db4f07dedbf6
Sanjay Khatri <sanzaikc@gmail.com>
2021-01-11 19:38:34 +0545

    base url added to env file

--- client_app/src/plugins/axios.js
+++ client_app/src/plugins/axios.js
@@ -1,11 +1,11 @@
 import axios from "axios";
-import store from "../store"
+import store from "../store";
 
 const $axios = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/"
+  baseURL: process.env.VUE_APP_API_BASE_URL,
 });
 
-$axios.interceptors.request.use(config => {
+$axios.interceptors.request.use((config) => {
   const token = store.getters.token;
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
