a4e7a2fcb86345b90aca6d9fec683f0d2477e469
JemyJenesh <xemejenesh@gmail.com>
2021-01-21 19:34:26 +0545

    name validation added

--- server/app/Http/Controllers/PlayerController.php
+++ server/app/Http/Controllers/PlayerController.php
@@ -2,18 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use App\Quiz;
-use App\Player;
 use App\Events\PlayerJoined;
+use App\Player;
+use App\Quiz;
 use Illuminate\Http\Request;
 
 class PlayerController extends Controller {
   public function create(Request $request) {
+    $quiz = Quiz::wherePin($request->pin)->first();
     $validData = $request->validate([
-      'name' => 'required|min:3',
+      'name' => 'required|min:3|unique:players,name,NULL,id,quiz_id,' . $quiz->id,
       'pin' => 'required',
     ]);
-    $quiz = Quiz::wherePin($request->pin)->first();
     if (!$quiz) {
       return response(['message' => 'Invalid pin!']);
     }
@@ -22,7 +22,7 @@ class PlayerController extends Controller {
       "quiz_id" => $quiz->id,
     ]);
     event(new PlayerJoined($player));
-    
+
     return response(['player' => $player]);
   }
 }
