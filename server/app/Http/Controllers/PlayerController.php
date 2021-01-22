49a67a5e0ea13a47f668ea491eb2cd95bce5a716
JemyJenesh <xemejenesh@gmail.com>
2021-01-22 15:19:21 +0545

    wrong pin error

--- server/app/Http/Controllers/PlayerController.php
+++ server/app/Http/Controllers/PlayerController.php
@@ -9,14 +9,14 @@ use Illuminate\Http\Request;
 
 class PlayerController extends Controller {
   public function create(Request $request) {
-    $quiz = Quiz::wherePin($request->pin)->firstOrFail();
+    $quiz = Quiz::wherePin($request->pin)->first();
+    if (!$quiz) {
+      return response(['message' => 'Invalid pin!']);
+    }
     $validData = $request->validate([
       'name' => 'required|min:3|unique:players,name,NULL,id,quiz_id,' . $quiz->id,
       'pin' => 'required',
     ]);
-    if (!$quiz) {
-      return response(['message' => 'Invalid pin!']);
-    }
     $player = Player::create([
       "name" => $request->name,
       "quiz_id" => $quiz->id,
