72f13c28e8f38e5e5a469277298844b1c9e4d21d
Sanjay Khatri <sanzaikc@gmail.com>
2021-01-25 08:25:23 +0545

    wip

--- client_app/src/components/game/GameBoard.vue
+++ client_app/src/components/game/GameBoard.vue
@@ -1,121 +1,176 @@
 <template>
   <div>
-    <div>
-      <!-- scoreboard if quiz has been started -->
-      <!-- else list of players -->
-      <pre>
-        {{players.length}}
-      </pre>
-    </div>
-    <div>
-      <!-- the question -->
-      <pre>
-        {{question.body}}
-      </pre>
-    </div>
-    <div>
-      <!-- Timer -->
-    </div>
+    <b-row class="my-5">
+      <b-col cols="12" lg="3">
+        <!-- scoreboard if quiz has been started -->
+        <!-- else list of players -->
+        <b-card v-if="quizStarted"> Scoreboard </b-card>
+      </b-col>
+      <b-col cols="12" lg="6">
+        <!-- the question -->
+        <div v-if="quizStarted">
+          <h3 style="text-align: center">
+            {{ question.body }}
+          </h3>
+
+          <div v-for="{ id, body } in options" :key="id">
+            <b-button
+              :variant="id == correctAnswer.option_id ? 'success' : ''"
+              class="my-2"
+            >
+              {{ body }}
+            </b-button>
+          </div>
+        </div>
+
+        <div v-else>
+          <h2 class="mb-2">Please wait for the host to start the quiz</h2>
+          <b-card v-if="!quizStarted">
+            Participants
+            <ul>
+              <li v-for="player in players" :key="player.id">
+                {{ player.name }} - {{ player.score }}
+              </li>
+            </ul>
+          </b-card>
+        </div>
+      </b-col>
+
+      <b-col cols="12" lg="3">
+        <div v-if="quizStarted">
+          <div v-if="!timesUp" style="font-size: 5rem">{{ time }}s</div>
+          <div v-else>Time is up</div>
+        </div>
+      </b-col>
+    </b-row>
   </div>
 </template>
 
 <script>
-import $axios from "@/plugins/axios";
+  import $axios from "@/plugins/axios";
 
-export default {
-  data() {
-    return {
-      quiz: null,
-    };
-  },
-
-  computed: {
-    quizId() {
-      return this.$route.params.quizId;
+  export default {
+    data() {
+      return {
+        quizStarted: false, //have to relay on better data for these variables
+        quizEnded: false,
+        time: 30,
+      };
     },
 
-    players() {
-      if (this.quiz) {
-        return this.quiz.players;
-      }
-      return [];
-    },
+    computed: {
+      quizId() {
+        return this.$route.params.quizId;
+      },
 
-    scores() {
-      if (this.quiz)
-        return this.players.map((player) => ({
-          id: player.id,
-          name: player.name,
-          score: player.score ? player.score.score : 0,
-        }));
-      return [];
-    },
-    question() {
-      if (this.quiz) return this.quiz.current_question;
-      return {};
-    },
-  },
-
-  mounted() {
-    // request for quiz details; this request's response will players, players' score, current question
-    this.fetchQuizDetails();
-
-    this.listenForPlayerJoining();
-    this.listenForQuestionChange();
-
-    this.listenForPlayerAnswer();
-    this.listenForPass();
-
-    this.listenForQuizEnd();
-  },
-
-  methods: {
-    fetchQuizDetails() {
-      $axios
-        .get(`/getQuizDetails/${this.quizId}`)
-        .then((res) => {
-          if (res.status === 200) {
-            this.quiz = res.data.quiz;
-          }
-        })
-        .catch((err) => console.log(err))
-        .finally(() => console.log("at last"));
-    },
-
-    listenForPlayerJoining() {
-      window.Echo.channel("quizy" + this.quizId).listen("PlayerJoined", (e) => {
-        this.quiz = { ...this.quiz, players: e.players };
-
-        this.$toasted.show(e.player.name + " joined!");
-      });
-    },
-
-    listenForPlayerAnswer() {},
-
-    listenForPass() {},
-
-    listenForQuestionChange() {
-      window.Echo.channel("quizy" + this.quizId).listen(
-        "QuestionChanged",
-        (e) => {
-          this.quiz = { ...this.quiz, current_question: e.question };
-          // this.quizStarted = true;
+      players() {
+        if (this.quiz) {
+          return this.quiz.players;
         }
-      );
+        return [];
+      },
+
+      scores() {
+        if (this.quiz)
+          return this.players.map((player) => ({
+            id: player.id,
+            name: player.name,
+            score: player.score ? player.score.score : 0,
+          }));
+        return [];
+      },
+
+      question() {
+        if (this.quiz) return this.quiz.current_question;
+        return {};
+      },
+
+      options() {
+        return this.question.options;
+      },
+
+      correctAnswer() {
+        return this.question.answer;
+      },
+
+      timesUp() {
+        return !this.time > 0;
+      },
     },
-    listenForQuizEnd() {
-      window.Echo.channel("quizy" + this.quizId).listen("QuizEnded", (e) => {
-        this.quizHasEnded = true;
-        this.REPLACE_PLAYERS(
-          e.quiz.players.map((player) => ({
-            ...player,
-            score: player.score.score,
-          }))
+
+    created() {
+      this.listenForPlayerJoining();
+
+      this.listenForQuestionChange();
+
+      this.listenForPlayerAnswer();
+
+      this.listenForPass();
+
+      this.listenForQuizEnd();
+    },
+
+    mounted() {
+      // request for quiz details; this request's response will return players, player's score, current question
+      this.fetchQuizDetails();
+    },
+
+    methods: {
+      fetchQuizDetails() {
+        $axios
+          .get(`/getQuizDetails/${this.quizId}`)
+          .then((res) => {
+            if (res.status === 200) {
+              this.quiz = res.data.quiz;
+            }
+          })
+          .catch((err) => console.log(err))
+          .finally(() => console.log("at last"));
+      },
+
+      listenForPlayerJoining() {
+        window.Echo.channel("quizy" + this.quizId).listen(
+          "PlayerJoined",
+          (e) => {
+            this.quiz = { ...this.quiz, players: e.players };
+
+            this.$toasted.show(e.player.name + " joined!");
+          }
         );
-      });
+      },
+
+      listenForQuestionChange() {
+        window.Echo.channel("quizy" + this.quizId).listen(
+          "QuestionChanged",
+          (e) => {
+            this.quiz = { ...this.quiz, current_question: e.question };
+            this.quizStarted = true;
+            this.startTimer();
+          }
+        );
+      },
+
+      listenForQuizEnd() {
+        window.Echo.channel("quizy" + this.quizId).listen("QuizEnded", (e) => {
+          this.quizEnded = true;
+          console.log(e);
+        });
+      },
+
+      listenForPlayerAnswer() {},
+
+      listenForPass() {},
+
+      startTimer() {
+        setTimeout(() => {
+          if (this.time > 0) {
+            --this.time;
+            this.startTimer();
+          }
+        }, 1000);
+      },
     },
-  },
-};
+  };
 </script>
 
 <style></style>
