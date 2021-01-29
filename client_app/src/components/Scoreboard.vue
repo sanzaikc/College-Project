745668ad8b0862093adca755a3e48b916c7f8daf
Sanjay Khatri <sanzaikc@gmail.com>
2021-01-29 08:53:24 +0545

    leaderboard refactored

--- client_app/src/components/Scoreboard.vue
+++ client_app/src/components/Scoreboard.vue
@@ -1,28 +1,37 @@
 <template>
   <div>
-    <b-card no-body header="Scoreboard" class="w-100">
-      <transition-group class="list-group list-group-flush" name="flip-list">
-        <b-list-group-item
-          :active="turnOf === score.id"
+    <div class="leaderboard-card">
+      <div class="leaderboard-heading">
+        Leaderboard
+      </div>
+
+      <transition-group class="scores" name="flip-list" tag="ul">
+        <li
+          name="flip-list"
           v-for="(score, index) in sortedArray"
+          tag="li"
           :key="score.id"
-          class="d-flex justify-content-between align-items-center"
+          class="player"
+          :class="turnOf === score.id ? 'playerTurn' : ''"
         >
-          <span class="m-0">
-            <b-badge
+          <div class="status">
+            <div>
+              {{ score.name }}
+            </div>
+            <div
               v-if="index < 3 && score.score > 0"
-              :variant="playerRank(index).color"
-              class="py-1 mr-2"
-              pill
+              class="badge"
+              :style="{ background: `${playerRank(index).color}` }"
             >
               {{ playerRank(index).rank }}
-            </b-badge>
-            {{ score.name }}
-          </span>
-          <b>{{ score.score }}</b>
-        </b-list-group-item>
+            </div>
+          </div>
+          <div class="score">
+            {{ score.score }}
+          </div>
+        </li>
       </transition-group>
-    </b-card>
+    </div>
   </div>
 </template>
 
@@ -41,6 +50,10 @@
         // eslint-disable-next-line vue/no-side-effects-in-computed-properties
         return this.scores.sort((a, b) => (a.score > b.score ? -1 : 1));
       },
+
+      leading() {
+        return this.sortedArray[0];
+      },
     },
 
     methods: {
@@ -49,17 +62,17 @@
           {
             index: 0,
             rank: "1st",
-            color: "success",
+            color: "green",
           },
           {
             index: 1,
             rank: "2nd",
-            color: "info",
+            color: "skyblue",
           },
           {
             index: 2,
             rank: "3rd",
-            color: "warning",
+            color: "orange",
           },
         ];
 
@@ -72,5 +85,71 @@
 <style>
   .flip-list-move {
     transition: transform 1s;
+  }
+
+  .current {
+    border-left: 2rem solid steelblue;
+  }
+
+  .leaderboard-card {
+    background: white;
+    border-radius: 0.25rem;
+
+    box-shadow: 0 2px 6px rgb(224, 210, 210);
+  }
+
+  .scores {
+    list-style-type: none;
+    padding: 0;
+    font-size: 1.1rem;
+  }
+
+  .score {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .player {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0.5rem;
+    border-left: 5px solid white;
+  }
+
+  .status {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-grow: 1;
+    font-weight: 600;
+    padding: 0 0.5rem;
+  }
+
+  .badge {
+    color: white;
+    font-size: 0.75rem;
+    border-radius: 2rem;
+    padding: 0.35rem 0.5rem;
+    width: 40px;
+    margin-right: 1rem;
+  }
+
+  .score {
+    font-size: 1.25rem;
+    font-weight: 500;
+    width: 30px;
+  }
+
+  .playerTurn {
+    border-left: 5px solid steelblue;
+  }
+
+  .leaderboard-heading {
+    padding: 0.5rem 1.25rem;
+    border-bottom: 1px solid lightgray;
+    font-size: 1.2rem;
+    font-weight: 600;
   }
 </style>
