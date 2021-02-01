a2d29947120aa8062608bac87ad5a34f848e790e
Sanjay Khatri <sanzaikc@gmail.com>
2021-02-01 19:43:55 +0545

    bug fixed

--- server/database/migrations/2020_07_24_150835_create_quizzes_table.php
+++ server/database/migrations/2020_07_24_150835_create_quizzes_table.php
@@ -4,34 +4,37 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateQuizzesTable extends Migration {
-  /**
-   * Run the migrations.
-   *
-   * @return void
-   */
-  public function up() {
-    Schema::create('quizzes', function (Blueprint $table) {
-      $table->id();
-      $table->string('name');
-      $table->text('description');
-      $table->string('image')->nullable();
-      $table->integer('pin')->nullable();
-      $table->bigInteger('current_question')->nullable();
-      $table->enum('round_type', ['timer', 'buzzer'])->default('timer');
-      $table->bigInteger('player_id')->nullable();
-      $table->boolean('can_answer')->default('true');
-      $table->foreignId('user_id')->constrained()->onDelete('cascade');
-      $table->timestamps();
-    });
-  }
+class CreateQuizzesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('quizzes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->string('image')->nullable();
+            $table->integer('pin')->nullable();
+            $table->bigInteger('current_question')->nullable();
+            $table->enum('round_type', ['timer', 'buzzer'])->default('timer');
+            $table->bigInteger('player_id')->nullable();
+            $table->boolean('can_answer')->default(true);
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
 
-  /**
-   * Reverse the migrations.
-   *
-   * @return void
-   */
-  public function down() {
-    Schema::dropIfExists('quizzes');
-  }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('quizzes');
+    }
 }
